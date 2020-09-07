f8cfd3aaf00b56e87f8998f17e5cff4b3b8256a4
lis0517 <43354156+lis0517@users.noreply.github.com>
2020-09-08 00:38:38 +0900

    create : project
    Property Script Add

---
+++ Lys-001/Assets/Scripts/Properties/Property.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityData.Biding.Internal
+{
+    public class Property 
+    {
+
+        private object _value;
+
+        public Property(object value)
+            => _value = value;
+
+        public Property() { }
+
+        /// <summary>
+        /// onValueChanged를 위한 델리게이트
+        /// </summary>
+        public delegate void ValueChangedDelegate();
+
+        /// <summary>
+        /// 프로피터가 변경된 후 호출
+        /// </summary>
+        public event ValueChangedDelegate ValueChanged;
+
+        public object Value
+        {
+            get => _value;
+            set
+            {
+                var isChanged = !Equals(_value, value);
+                if (!isChanged) return;
+                _value = value;
+                ValueChanged?.Invoke();
+            }
+        }
+    }
+}
---
+++ Lys-001/Assets/Scripts/Properties/PropertyFinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UnityData.Biding.Internal
+{
+    public static class PropertyFinder
+    {
+
+        private static readonly HashSet<Type> hiddenTypes = new HashSet<Type>
+        {
+            //typeof(Abas)
+        };
+        
+    }
+}
+
